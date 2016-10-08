db585fb572544a1ca99207c3b2b9faf97a7702ef
DimitrYo <dimitr.yovchev@gmail.com>
2016-10-08 12:40:42 +0300

    Added find_abbr and find_eng

--- premorph.py
+++ premorph.py
@@ -7,7 +7,8 @@ import regex as re
 
 SENTENCES_RE = '[\.\!\?]+'
 WORDS_RE = '[^[:alnum:]]+'
-
+ENG_RE = u"[a-zA-Z]{2,}"
+ABBR_RE = u"[\u0410-\u042F]{2,}"
 
 def split_with_rexp(text, rsplt):
     sentences = re.split(rsplt, text.lower())
@@ -24,6 +25,25 @@ def split_by_sentences(text):
 
 def split_by_words(text):
     return filter(lambda x: bool(x), split_with_rexp(text, WORDS_RE))
+
+
+def find_abbr(sentence):
+    """
+    :param sentence:
+    :return:
+    """
+    abbr_list = re.findall(ABBR_RE, sentence)  # А-Я
+    return abbr_list
+
+def find_eng(sentence):
+    """
+
+    :param sentence:
+    :return:
+    """
+    eng_list = re.findall(ENG_RE, ukr)  # А-Я
+    return eng_list
+
 
 
 def premorph(text):
